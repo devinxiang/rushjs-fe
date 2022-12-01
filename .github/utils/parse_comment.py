bda324fdb452b2cf464678375f1e67d51444f630
devinxl <devin@nodereal.io>
2022-12-01 21:39:48 +0800

    feat: test

--- .github/utils/parse_comment.py
+++ .github/utils/parse_comment.py
@@ -3,7 +3,15 @@ import json
 
 comment_body = os.environ.get('COMMENT_BODY')
 temp = comment_body.split(':')
+
 res = temp[1].split(',')
-res_str = ' '.join(res)
+res_str = ','.join(res)
+
+res_json = {
+	'name': temp[0],
+	'labels': temp[1]
+}
+res_json_str = json.dumps
 
 print(f"::set-output name=COMMENT_LABELS::{res_str}")
+print(f"::set-output name=COMMENT_JSON::{res_json_str}")
