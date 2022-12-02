b4f99124c1e3c6681a22048d0860b489f4e15c82
devinxl <devin@nodereal.io>
2022-12-02 08:01:12 +0800

    feat: add build id

--- apps/nextjs-start/next.config.js
+++ apps/nextjs-start/next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
+let commitHash = 'no-git-commit';
+try {
+  commitHash = process.env.COMMIT_SHA || 'no-git-commit';
+} catch (error) {
+  console.error(`Get git commit hash failed.`);
+}
+
+console.log('commitHash', commitHash)
+const prod = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  generateBuildId: async () => {
+    return commitHash
+  },
 }
 
 module.exports = nextConfig
