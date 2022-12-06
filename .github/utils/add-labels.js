b7e812259e4a4b49ee9e15cb5251cb92a7971ed8
devinxl <devin@nodereal.io>
2022-12-06 10:31:20 +0800

    feat: update script

--- .github/utils/add-labels.js
+++ .github/utils/add-labels.js
@@ -1,10 +1,10 @@
-module.exports = async ({github, context}) {
+module.exports = async ({github, context}) => {
 	const str = "${{ github.event.comment.body }}";
 	const arr = str.split(':')
 	const labels = arr[1].split(',');
 
 	if (arr[0] === '/add-labels') {
-		github.rest.issues.addLabels({
+		await github.rest.issues.addLabels({
 			issue_number: context.issue.number,
 			owner: context.repo.owner,
 			repo: context.repo.repo,
