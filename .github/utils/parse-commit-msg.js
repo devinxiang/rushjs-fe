95724fb840aa0a765550c2449adc4c52ddf1d18e
devinxl <devin@nodereal.io>
2022-12-06 19:51:24 +0800

    feat: Add labels

---
+++ .github/utils/parse-commit-msg.js
@@ -0,0 +1,13 @@
+module.export = async ({ diffMsgs, core }) => {
+	try {
+		const commits = diffMsgs.split('/n').filter(m => !/^Merge pull request/.test(m))
+			.map(m => m.replace(/\n+(.*)/g, '\n> $1'))
+			.map(m => `> ${m}`)
+			.join('\n');
+
+		core.setOutput('commits', commits)
+	} catch (error) {
+		core.error(error.message)
+	}
+
+}
--- .github/utils/parse-commit-msg.mjs
+++
@@ -1,8 +0,0 @@
-import {core} from '@actions'
-
-export default async function ParseCommitMsg({ commitMsgs }) {
-	const commits = commitMsgs.split('/n').filter(m => !/^Merge pull request/.test(m))
-	.map(m => m.replace(/\n+(.*)/g, '\n> $1'))
-	.map(m => `> ${m}`)
-	.join('\n')
-}
