b30491d7fe071516221710703b58c1fdd1c9cb5f
devinxl <devin@nodereal.io>
2022-12-06 10:02:28 +0800

    feat: Add labels

---
+++ .github/utils/add-labels.js
@@ -0,0 +1,14 @@
+export default function CreateDeployLabel({github, context}) {
+	const str = "${{ github.event.comment.body }}";
+	const arr = str.split(':')
+	const labels = arr[1].split(',');
+
+	if (arr[0] === '/add-labels') {
+		github.rest.issues.addLabels({
+			issue_number: context.issue.number,
+			owner: context.repo.owner,
+			repo: context.repo.repo,
+			labels: labels
+		})
+	}
+}
