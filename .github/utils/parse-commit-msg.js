3797ef2fe1ed6becf0d2acad91a95facf6a4d31e
devinxl <devin@nodereal.io>
2022-12-09 15:10:56 +0800

    feat(meganode-ui): Update meganode workflow

--- .github/utils/parse-commit-msg.js
+++ .github/utils/parse-commit-msg.js
@@ -2,12 +2,11 @@
 module.exports = async ({ diffMsgs, core }) => {
 	console.log('diffMsgs-333333', diffMsgs, typeof diffMsgs);
 	try {
-		const commits = `${diffMsgs}`.split('\n').filter(
+		const commits = diffMsgs.split('\n').filter(
 				(m) => !/^(Merge pull request|Merge branch|Merge remote-tracking)/.test(m) && !!m
 			)
 			.map((m) => `- ${m}`)
 			.map((m) => m.replace(/\n+(.*)/g, '\n> $1'))
-			.unshift('# All Changes')
 			.join('\n');
 
 		core.setOutput('commits', commits)
